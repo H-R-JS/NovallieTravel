6a856740dc5be0d846594ba6703f8fa08e8e4dc3
WhitRavn <jmr4277@orange.fr>
2022-10-17 21:37:26 +0200

    js modif

--- novalltravel/src/Components/BodyAll/Section1.jsx
+++ novalltravel/src/Components/BodyAll/Section1.jsx
@@ -1,49 +1,57 @@
 import React from "react";
 
-const allBoxs = [
-  { section: "section-category r", title: "title-box1" },
-  { section: "section-category v", title: "title-box2" },
-  { section: "section-category h", title: "title-box3" },
-  { section: "section-category m", title: "title-box4" },
-];
-
 class Boxs extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      section1: "section-category r",
-      title1: "title-box1",
-      text1: "Randonnées",
-      section2: "section-category v",
-      title2: "title-box2",
-      text2: "Voyage",
+      section1: {
+        class1: "section-category r",
+        title1: "title-box1",
+        text1: "Randonnées",
+      },
+      section2: {
+        class2: "section-category v",
+        title2: "title-box2",
+        text2: "Voyage",
+      },
     };
     this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick() {
-    if (this.state.section1 === "section-category r") {
+    if (this.state.section1.class1 === "section-category r") {
       this.setState({
-        section1: "section-category h",
-        title1: "title-box3",
-        text1: "Hôtel",
-        section2: "section-category m",
-        title2: "title-box4",
-        text2: "Mer",
+        section1: {
+          class1: "section-category h",
+          title1: "title-box3",
+          text1: "Hôtel",
+        },
+
+        section2: {
+          class2: "section-category m",
+          title2: "title-box4",
+          text2: "Mer",
+        },
       });
-    } else if (this.state.section1 === "section-category h") {
+    } else if (this.state.section1.class1 === "section-category h") {
       this.setState({
-        section1: "section-category r",
-        title1: "title-box1",
-        text1: "Randonnées",
-        section2: "section-category v",
-        title2: "title-box2",
-        text2: "Voyage",
+        section1: {
+          class1: "section-category r",
+          title1: "title-box1",
+          text1: "Randonnées",
+        },
+        section2: {
+          class2: "section-category v",
+          title2: "title-box2",
+          text2: "Voyage",
+        },
       });
     }
   }
 
   render() {
+    const { class1, title1, text1 } = this.state.section1;
+    const { class2, title2, text2 } = this.state.section2;
     return (
       <div>
         <span
@@ -54,10 +62,10 @@ class Boxs extends React.Component {
           arrow_circle_left
         </span>
         <div className="div-all-box">
-          <div className={this.state.section1}></div>
-          <p className={this.state.title1}>{this.state.text1}</p>
-          <div className={this.state.section2}></div>
-          <p className={this.state.title2}>{this.state.text2}</p>
+          <div className={class1}></div>
+          <p className={title1}>{text1}</p>
+          <div className={class2}></div>
+          <p className={title2}>{text2}</p>
         </div>
         <span
           class="material-symbols-outlined"
--- novalltravel/src/Components/HeaderAll/Img.jsx
+++ novalltravel/src/Components/HeaderAll/Img.jsx
@@ -1,13 +1,42 @@
 import React from "react";
 
+class Image extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      source:
+        "https://scontent-sjc3-1.xx.fbcdn.net/v/t1.15752-9/311000440_668342541314593_2729506177059028220_n.jpg?stp=dst-jpg_p1080x2048&_nc_cat=102&ccb=1-7&_nc_sid=ae9488&_nc_ohc=yqogzRCxGlcAX-dqOTe&_nc_ht=scontent-sjc3-1.xx&oh=03_AdQ48a3sadXZbzEt7VmkJl6kfWCwok5YPdhWwVCHiS-y5A&oe=63739D87",
+    };
+    this.changeImg = this.changeImg.bind(this);
+  }
+
+  changeImg() {
+    setInterval(() => {
+      this.setState({
+        source:
+          "https://scontent-cdt1-1.xx.fbcdn.net/v/t1.15752-9/310451613_352991217002664_2159071637651135923_n.jpg?stp=dst-jpg_p1080x2048&_nc_cat=103&ccb=1-7&_nc_sid=ae9488&_nc_ohc=ruJ2IBh8UrwAX9AzpY4&_nc_ht=scontent-cdt1-1.xx&oh=03_AdRpfCS-ZeAGDjMsH7jfLFUEO4MnWJOi6ybRdfBOtKk3ig&oe=636D5126",
+      });
+    }, 2000);
+  }
+
+  render() {
+    return (
+      <div>
+        <img
+          className="img"
+          src={this.state.source}
+          changeOn={this.changeImg}
+          alt="Lac blaussee de suisse, avec un bateau rouge et des poissons dans le lac"
+        ></img>
+      </div>
+    );
+  }
+}
+
 export function Img() {
   return (
     <div>
-      <img
-        className="img"
-        src="https://scontent-cdt1-1.xx.fbcdn.net/v/t1.15752-9/310451613_352991217002664_2159071637651135923_n.jpg?stp=dst-jpg_p1080x2048&_nc_cat=103&ccb=1-7&_nc_sid=ae9488&_nc_ohc=ruJ2IBh8UrwAX9AzpY4&_nc_ht=scontent-cdt1-1.xx&oh=03_AdRpfCS-ZeAGDjMsH7jfLFUEO4MnWJOi6ybRdfBOtKk3ig&oe=636D5126"
-        alt="Lac blaussee de suisse, avec un bateau rouge et des poissons dans le lac"
-      ></img>
+      <Image />
     </div>
   );
 }
