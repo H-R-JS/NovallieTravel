db1a77e46ef9791e23721fc560a8796a1309cf81
WhitRavn <jmr4277@orange.fr>
2022-10-20 18:28:14 +0200

    add animation but modification Boxs

---
+++ novalltravel/src/Components/BodyAll/AllBoxs.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+export function Boxs() {
+  const boxs = React.Children;
+}
+
+export function Box() {}
+
+/*  const boxs1 = [
+    {
+      className: "section-category r",
+      title: "title-box1",
+      children: "Randonnées",
+    },
+    {
+      className: "section-category v",
+      title: "title-box2",
+      children: "Voyage",
+    },
+  ];
+  const [currentBoxs, setCurrentBoxs] = useState(boxs1);
+
+  return (
+    <div>
+      <div className="div-all-box">
+        <div className={currentBoxs[0].className}></div>
+        <p className={currentBoxs[0].title}>{currentBoxs[0].children}</p>
+        <div className={currentBoxs[1].className}></div>
+        <p className={currentBoxs[1].title}>{currentBoxs[1].children}</p>
+      </div>
+      <div className="text-end-section"></div>
+    </div>
+  );*/
--- novalltravel/src/Components/BodyAll/Section1.jsx
+++ novalltravel/src/Components/BodyAll/Section1.jsx
@@ -1,10 +1,70 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Boxs } from "./AllBoxs";
+import { Box } from "./AllBoxs";
 
-class Boxs extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      section1: {
+function TransitionBoxs({ visible, children }) {
+  const [showChildren, setShowChildren] = useState(visible);
+
+  useEffect(() => {
+    if (visible) {
+      setShowChildren(true);
+    } else {
+      const timer = setTimeout(() => {
+        setShowChildren(false);
+      }, 300);
+      return () => {
+        clearTimeout(timer);
+      };
+    }
+  }, [visible]);
+
+  let className = "fade";
+  if (!visible) {
+    className += " out";
+  }
+
+  return <div className={className}>{showChildren && children}</div>;
+}
+
+export function Section1() {
+  const [open, setOpen] = useState(true);
+
+  const toggle = () => setOpen((o) => !o);
+  return (
+    <div className="section1-background">
+      <div className="section-title-all">
+        <h2>Nos Voyages</h2>
+        <p>Cliquez sur la section qui vous intéresse</p>
+      </div>
+      <span
+        className={"material-symbols-outlined"}
+        id="arrow-left"
+        onClick={toggle}
+      >
+        arrow_circle_left
+      </span>
+      <TransitionBoxs visible={open}>
+        <Boxs>
+          <Box></Box>
+          <Box></Box>
+        </Boxs>
+      </TransitionBoxs>
+      <span
+        className="material-symbols-outlined"
+        id="arrow-right"
+        onClick={toggle}
+      >
+        arrow_circle_right
+      </span>
+    </div>
+  );
+}
+
+/* NOTE A FAIRE !!!!
+ - Pour les section, le mieux serait de mettre directement les elements dans le state si possible, le div directement
+ - Pour les sectons, trouver un moyen d'activer les transition avec opacity et translation
+
+section1: {
         class1: "section-category r",
         title1: "title-box1",
         text1: "Randonnées",
@@ -14,14 +74,7 @@ class Boxs extends React.Component {
         title2: "title-box2",
         text2: "Voyage",
       },
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick() {
-    if (this.state.section1.class1 === "section-category r") {
-      this.setState({
-        section1: {
+      section1: {
           class1: "section-category h",
           title1: "title-box3",
           text1: "Hôtel",
@@ -32,69 +85,4 @@ class Boxs extends React.Component {
           title2: "title-box4",
           text2: "Mer",
         },
-      });
-    } else if (this.state.section1.class1 === "section-category h") {
-      this.setState({
-        section1: {
-          class1: "section-category r",
-          title1: "title-box1",
-          text1: "Randonnées",
-        },
-        section2: {
-          class2: "section-category v",
-          title2: "title-box2",
-          text2: "Voyage",
-        },
-      });
-    }
-  }
-
-  render() {
-    const { class1, title1, text1 } = this.state.section1;
-    const { class2, title2, text2 } = this.state.section2;
-    return (
-      <div>
-        <span
-          className={"material-symbols-outlined"}
-          id="arrow-left"
-          onClick={this.handleClick}
-        >
-          arrow_circle_left
-        </span>
-        <div className="div-all-box">
-          <div className={class1}></div>
-          <p className={title1}>{text1}</p>
-          <div className={class2}></div>
-          <p className={title2}>{text2}</p>
-        </div>
-        <span
-          className="material-symbols-outlined"
-          id="arrow-right"
-          onClick={this.handleClick}
-        >
-          arrow_circle_right
-        </span>
-        <div className="text-end-section"></div>
-      </div>
-    );
-  }
-}
-
-export function Section1() {
-  return (
-    <div className="section1-background">
-      <div className="section-title-all">
-        <h2>Nos Voyages</h2>
-        <p>Cliquez sur la section qui vous intéresse</p>
-      </div>
-      <Boxs />
-    </div>
-  );
-}
-
-/* NOTE A FAIRE !!!!
- - Pour les section, le mieux serait de mettre directement les elements dans le state si possible, le div directement
- - Pour les sectons, trouver un moyen d'activer les transition avec opacity et translation
-
-
 */
