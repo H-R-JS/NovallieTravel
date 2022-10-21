5d9549696af567273afa6fe917b3a6b21706c3a8
WhitRavn <jmr4277@orange.fr>
2022-10-21 19:54:57 +0200

    add boxs and box, but modification are hard

--- novalltravel/src/Components/BodyAll/AllBoxs.jsx
+++ novalltravel/src/Components/BodyAll/AllBoxs.jsx
@@ -1,10 +1,35 @@
 import React, { useState } from "react";
 
-export function Boxs() {
-  const boxs = React.Children;
+export function Boxs({ children }) {
+  const boxs = React.Children.toArray(children);
+  console.log(boxs);
+  const [current, setCurrent] = useState(boxs[(0, 1)].key);
+  const newBoxs1 = boxs.map((box) => {
+    return React.cloneElement(box, { className: current.className });
+  });
+
+  function toggleBoxs() {
+    // développer un changement de boite
+  }
+  return (
+    <div>
+      <span className={"material-symbols-outlined"} id="arrow-left">
+        arrow_circle_left
+      </span>
+      {boxs.map((box) => (
+        <div key={box.key} className={box.props.className}></div>
+      ))}
+      <span className="material-symbols-outlined" id="arrow-right">
+        arrow_circle_right
+      </span>
+      <section>{newBoxs1}</section>
+    </div>
+  );
 }
 
-export function Box() {}
+export function Box({ children }) {
+  return <div>{children}</div>;
+}
 
 /*  const boxs1 = [
     {
--- novalltravel/src/Components/BodyAll/Section1.jsx
+++ novalltravel/src/Components/BodyAll/Section1.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Boxs } from "./AllBoxs";
-import { Box } from "./AllBoxs";
+import { Boxs, Box } from "./AllBoxs";
 
 function TransitionBoxs({ visible, children }) {
   const [showChildren, setShowChildren] = useState(visible);
@@ -29,33 +28,23 @@ function TransitionBoxs({ visible, children }) {
 export function Section1() {
   const [open, setOpen] = useState(true);
 
-  const toggle = () => setOpen((o) => !o);
   return (
     <div className="section1-background">
       <div className="section-title-all">
         <h2>Nos Voyages</h2>
         <p>Cliquez sur la section qui vous intéresse</p>
       </div>
-      <span
-        className={"material-symbols-outlined"}
-        id="arrow-left"
-        onClick={toggle}
-      >
-        arrow_circle_left
-      </span>
+
       <TransitionBoxs visible={open}>
         <Boxs>
-          <Box></Box>
-          <Box></Box>
+          <Box className="section-category r" selected>
+            <p className="title-box1">Randonnée</p>
+          </Box>
+          <Box className="section-category v" selected>
+            <p className="title-box2">Voyages</p>
+          </Box>
         </Boxs>
       </TransitionBoxs>
-      <span
-        className="material-symbols-outlined"
-        id="arrow-right"
-        onClick={toggle}
-      >
-        arrow_circle_right
-      </span>
     </div>
   );
 }
@@ -67,7 +56,7 @@ export function Section1() {
 section1: {
         class1: "section-category r",
         title1: "title-box1",
-        text1: "Randonnées",
+        text1: "Randonnée",
       },
       section2: {
         class2: "section-category v",
