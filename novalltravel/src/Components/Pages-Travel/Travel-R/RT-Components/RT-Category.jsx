a8a453efffb514e738c4d52c67ccd08b5272f048
WhitRavn <jmr4277@orange.fr>
2022-11-11 17:54:25 +0100

    add second category

--- novalltravel/src/Components/Pages-Travel/Travel-R/RT-Components/RT-Category.jsx
+++ novalltravel/src/Components/Pages-Travel/Travel-R/RT-Components/RT-Category.jsx
@@ -1,28 +1,45 @@
 import React from "react";
 
-const FirstArrayRT = [
-  "Pyrénées-Atlantiques",
-  "Hautes-Pyrénées",
-  "Hautes-Garonne",
-  "Ariège",
-  "Pyrénées-Orientales",
+const CategoryArrayRT = [
+  { firstCat: "Pyrénées-Atlantiques", secondCat: "lac de montagnon(Aydius)" },
+  {
+    firstCat: "Hautes-Pyrénées",
+    secondCat: [
+      "lac des glorittes(Gavarnie-Gèdre)",
+      "cirque de Gavarnie",
+      "lac de l'oule(Aragnouet)",
+      "lac d'Estom (Cauterets)",
+      "lac de Bareille (Bourg-d'Oueil)",
+    ],
+  },
+  { firstCat: "Hautes-Garonne", secondCat: "lac d'Ôo (Ôo)" },
+  {
+    firstCat: "Pyrénées-Orientales",
+    secondCat: "Gorge de Calamus (Saint-antoine de Galamus)",
+  },
 ];
-
-const SecondArrayRT = [];
+/*"Ariège" */
 
 export class CategoryRT extends React.Component {
   renderChildrenView = (item, index) => {
     return (
-      <div className="content-first-category" key={index}>
-        <p>{item}</p>
+      <div key={index}>
+        <div className="first-category">
+          <p className="content-first-category">{item.firstCat}</p>
+        </div>
+        <div className="second-category">
+          <p className="content-second-category">{item.secondCat}</p>
+        </div>
       </div>
     );
+
+    /**Mettre un index dans la seconde cat pour que les valeur du tableau soit indépendant */
   };
 
   render() {
     return (
       <div className="all-category-rt">
-        <FirstCategoryRT dataArray={FirstArrayRT}>
+        <FirstCategoryRT dataArray={CategoryArrayRT}>
           {this.renderChildrenView}
         </FirstCategoryRT>
       </div>
@@ -34,13 +51,9 @@ class FirstCategoryRT extends React.Component {
   render() {
     const { dataArray } = this.props;
     return (
-      <div className="">
+      <div>
         {dataArray.map((catego, index) => {
-          return (
-            <div key={index} className="first-category">
-              {this.props.children(catego, index)}
-            </div>
-          );
+          return <div key={index}>{this.props.children(catego, index)}</div>;
         })}
       </div>
     );
